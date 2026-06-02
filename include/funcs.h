8053711664cb91dca684b437b4ee5f08e372df40
Rasil Zargishiev <zargishiyev@gmail.com>
2026-06-02 12:52:33 +0000

    Document public interfaces
    Add comments to function declarations in header files.

--- include/funcs.h
+++ include/funcs.h
@@ -1,20 +1,28 @@
 #ifndef FUNCS_H
 #define FUNCS_H
 
+/* Function of one double argument returning double. */
 typedef double afunc(double);
 
+/* Compute f1(x) = 0.6 * x + 3. */
 double f1(double x);
+
+/* Compute f2(x) = (x - 2)^3 - 1. */
 double f2(double x);
+
+/* Compute f3(x) = 3 / x. */
 double f3(double x);
 
+/* Find intersection of two functions and compute iteration count. */
 double root_iterations(
-    afunc *f, 
+    afunc *f,
     afunc *g,
-    double a, 
+    double a,
     double b,
     double eps,
     int *iterations);
 
+/* Find intersection of two functions. */
 double root(
     afunc *f,
     afunc *g,
@@ -22,6 +30,7 @@ double root(
     double b,
     double eps);
 
+/* Compute definite integral of a function. */
 double integral(
     afunc *f,
     double a,
--- include/options.h
+++ include/options.h
@@ -1,9 +1,13 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
+/* Print command line usage information. */
 void print_help(const char *program);
 
+/* Run root-finding test specified by command line argument. */
 int test_root_option(const char *arg);
+
+/* Run integration test specified by command line argument. */
 int test_integral_option(const char *arg);
 
 #endif
